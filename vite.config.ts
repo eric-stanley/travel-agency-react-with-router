653679a665799ae6e201f8773e588a19835e9818
Eric Stanley <eric.stanleyjones@gmail.com>
2025-01-29 14:01:25 +0530

    updated vite condig to exclude dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,31 @@
 import path from "path-browserify";
-import { reactRouter } from "@react-router/dev/vite";
+import {
+    reactRouter
+} from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
+import {
+    defineConfig
+} from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
     },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
-  server: {
-    open: true,
-  },
-  ssr: {
-    // Add any SSR-specific settings here
-    noExternal: ["react-icons"], // Example of including external dependencies
-    external: ["gray-matter", "path-browserify", "react-icons", "react-markdown", "remark", "remark-html", "shadcn", "tailwindcss-animate"
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [reactRouter(), tsconfigPaths()],
+    server: {
+        open: true,
     },
-  },
+    ssr: {
+        // Add any SSR-specific settings here
+        noExternal: ["react-icons"], // Example of including external dependencies
+        external: ["gray-matter", "path-browserify", "react-icons", "react-markdown", "remark", "remark-html", "shadcn", "tailwindcss-animate"]
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
