696c9eab0dea7b6338872a7861c8cfa779fb4d72
Eric Stanley <eric.stanleyjones@gmail.com>
2025-01-29 14:13:40 +0530

    updated vite config to exclude dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
     ssr: {
         // Add any SSR-specific settings here
-        noExternal: ["react-icons"], // Example of including external dependencies
+        // noExternal: ["react-icons"], // Example of including external dependencies
         external: ["gray-matter", "path-browserify", "react-icons", "react-markdown", "remark", "remark-html", "shadcn", "tailwindcss-animate", "lucide-react"]
     },
     resolve: {
